3a7e56e7b025cd764eeda2c15f4ee2324a6d485a
NamanKhanna27 <namank27102004@gmail.com>
2025-12-14 06:30:21 -0700

    create earnings page with component wrapper

---
+++ work-shift-pay-tracker/app/earnings/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import EstimatedEarningsForm from "../components/EstimatedEarningsComponents/EstimatedEarningsForm";
+
+export default function EarningsPage() {
+  return (
+    <div className="p-6">
+      <EstimatedEarningsForm />
+    </div>
+  );
+}

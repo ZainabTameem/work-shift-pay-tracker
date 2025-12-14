eef0ad10c188604ecb11084bb8e349bcd850e762
NamanKhanna27 <namank27102004@gmail.com>
2025-12-14 08:26:16 -0700

    add base component structure for EstimatedEarningsForm

---
+++ work-shift-pay-tracker/app/components/EstimatedEarningsComponents/EstimatedEarningsForm.js
@@ -0,0 +1,13 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function EstimatedEarningsForm() {
+  return (
+    <div className="max-w-4xl mx-auto mt-10">
+      <h1 className="text-2xl font-bold text-[#0E4C58] mb-6">
+        Estimated Weekly Earnings
+      </h1>
+    </div>
+  );
+}

e9b69a1271798da78f6899692d64a98faba1f69a
NamanKhanna27 <namank27102004@gmail.com>
2025-12-14 08:33:36 -0700

    add hourly, overtime and shift state variables

--- work-shift-pay-tracker/app/components/EstimatedEarningsComponents/EstimatedEarningsForm.js
+++ work-shift-pay-tracker/app/components/EstimatedEarningsComponents/EstimatedEarningsForm.js
@@ -6,6 +6,9 @@ import { collection, onSnapshot, doc, getDoc } from "firebase/firestore";
 
 
 export default function EstimatedEarningsForm() {
+  const [hourlyWage, setHourlyWage] = useState(0);
+  const [overtimeWage, setOvertimeWage] = useState(0);
+  const [shifts, setShifts] = useState([]);
   return (
     <div className="max-w-4xl mx-auto mt-10">
       <h1 className="text-2xl font-bold text-[#0E4C58] mb-6">

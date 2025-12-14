1dac5878ae8c85e688d544e97b6a0384031fe990
NamanKhanna27 <namank27102004@gmail.com>
2025-12-14 06:30:55 -0700

    add base EstimatedEarningsForm component

---
+++ work-shift-pay-tracker/app/components/EstimatedEarningsComponents/EstimatedEarningsForm.js
@@ -0,0 +1,17 @@
+"use client";
+
+export default function EstimatedEarningsForm() {
+  return (
+    <div className="max-w-4xl mx-auto mt-10 bg-white shadow-md p-6 rounded-xl">
+      <h2 className="text-2xl font-semibold text-[#0E4C58] mb-6">
+        Weekly Earnings
+      </h2>
+
+      {/* NEW CODE WILL BE ADDED SECTION BY SECTION IN FUTURE COMMITS */}
+
+      <p className="text-center text-gray-500">
+        Earnings calculator coming soon...
+      </p>
+    </div>
+  );
+}
